af14bdb706ce67e702ce062e6436496ffc1f7117
Victor Arias <victor.j.arias@gmail.com>
2021-11-04 08:55:09 -0600

    Update Signify Config

---
+++ lua/signify-config.lua
@@ -0,0 +1,5 @@
+--Configuration for Signify 
+vim.cmd [[let g:signify_sign_add               = '+']]
+vim.cmd [[let g:signify_sign_delete            = '_']]
+vim.cmd [[let g:signify_sign_delete_first_line = '‾']]
+vim.cmd [[let g:signify_sign_change            = '~']]
